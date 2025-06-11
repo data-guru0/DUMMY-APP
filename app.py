28b341322b111074151274cd9615158b3c5b2f7c
Data Guru admin <gyrogodnon@gmail.com>
2025-06-11 09:48:45 +0530

    commit'

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
+REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    REQUEST_COUNT.inc()
+    return "<h1>Welcome to My Flask App 🚀</h1>"
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
